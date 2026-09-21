90489df58928f7477ada776ad20935d47773a192
Gabriela <gabriela.liz@unidavi.edu.br>
2026-09-21 19:16:58 -0300

    feat: estrutura inicial da API e Dockerfile

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.json({
+    status: "Sucesso",
+    message: "Aplicação SSDLC rodando perfeitamente!",
+    membros: ["Gabriela", "Integrante 2"]
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+}); 
